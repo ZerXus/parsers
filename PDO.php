454b1be2f5e9c18d3e33d329c04de0980002dae7
zerxus <sanya12092001@gmail.com>
2021-11-20 03:55:57 +0500

    Initial commit

---
+++ PDO.php
@@ -0,0 +1,19 @@
+<?php
+ini_set('display_errors', '1');
+
+const DB_USERNAME = 'u681963_orbita';
+const DB_PASSWORD = 'bT7lA2nP3lkS7m';
+const DB_DATABASE = 'u681963_orbita';
+const DB_REALRADIO_DATABASE = 'u681963_realradio';
+const DB_TDSPORTAL_DATABASE = 'u681963_tdsportal';
+
+function connectToDatabase ($db, $user, $password)
+{
+    $host = 'localhost';
+    $charset = 'utf8';
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $pdo = new \PDO($dsn, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $pdo;
+}
